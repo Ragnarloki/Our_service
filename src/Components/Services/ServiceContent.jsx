8391bcd20df28432e669060b8a0ea3a9575561ff
Ragnarloki <sivasankarswaminathan62@gmail.com>
2024-09-06 20:24:55 +0530

    commited

--- src/Components/Services/ServiceContent.jsx
+++ src/Components/Services/ServiceContent.jsx
@@ -17,7 +17,7 @@ function ServiceContent() {
     <div className=' text-white  '>
         {/* top-Content */}
         <div className='flex justify-evenly  mb-[30px] md:p-4 md:px-[30px] max-sm:p-2 max-sm:w-full max-sm:flex-col-reverse'>
-            <div className='flex flex-col md:mr-10 justify-center max-sm:w-full max-sm:mt-[20px]'>
+            <div className='p-2 md:p-0 flex flex-col md:mr-10 justify-center max-sm:w-full max-sm:mt-[20px]'>
                 <h1 className='text-5xl font-bold max-sm:text-xl'>Our Services</h1>
                 <p className='text-xl font-bold mt-2 max-sm:text-md max-sm:font-thin'>
                 We operate our banking services in many countries 
@@ -36,11 +36,11 @@ function ServiceContent() {
         {/* aboutus image and content */}
         <div className='bg-white text-black md:p-4 md:px-[30px] max-sm:p-[10px]'>
             <div className='flex items-center max-sm:flex-col'>
-                <img src={ourService} className='h-[400px] max-sm:h-[300px]'/>
+                <img src={ourService} className='h-[400px] max-sm:h-[300px] my-5 md:my-0'/>
                 <div className='space-y-4'>
-                    <h1 className='text-2xl font-bold text-[#4D4D4D]'>Our Services</h1>
+                    <h1 className='text-3xl font-bold text-[#4D4D4D] '>Our Services</h1>
                     <hr className='h-[5px] bg-[#4D4D4D]  rounded-md'/>
-                    <p className='text-justify'>Headquartered in Mumbai, L&T Finance Limited (formerly known as L&T Finance Holdings Limited) is one of the leading Non-Banking Financial Companies in India, with a strong presence across the lending business catering to the diverse financing needs of served and underserved customers.
+                    <p className='text-justify text-xl'>Headquartered in Mumbai, L&T Finance Limited (formerly known as L&T Finance Holdings Limited) is one of the leading Non-Banking Financial Companies in India, with a strong presence across the lending business catering to the diverse financing needs of served and underserved customers.
                         L&T Finance offers Personal loans, Two-Wheeler loans, Home loans, Rural Business loans, Farm loans, and SME loans. At L&T Finance, we are committed to providing customised financial solutions that meet the unique needs of our customers. With a focus on innovation and customer-centricity, we aim to be a trusted partner in our customers' financial journey.
                     </p>
                 </div>
@@ -49,14 +49,17 @@ function ServiceContent() {
         </div>
         <div className='bg-white text-black md:p-4 md:px-[30px] max-sm:p-[10px]'>
             <div className='flex items-center max-sm:flex-col'>                
+               <img src={collab} className='h-[400px]  max-sm:h-[300px] my-5 md:my-0 md:hidden p-2'/>
+            
                 <div className='space-y-4'>
-                    <h1 className='text-2xl font-bold text-[#4D4D4D]'>Why Us?</h1>
-                    <hr className='h-[5px] bg-[#4D4D4D]  rounded-md'/>
-                    <p className='text-justify'>Headquartered in Mumbai, L&T Finance Limited (formerly known as L&T Finance Holdings Limited) is one of the leading Non-Banking Financial Companies in India, with a strong presence across the lending business catering to the diverse financing needs of served and underserved customers.
+
+                    <h1 className='text-3xl font-bold text-[#4D4D4D]'>Why Us?</h1>
+                    <hr className='h-[5px] bg-[#4D4D4D]  rounded-md '/>
+                    <p className='text-justify text-xl'>Headquartered in Mumbai, L&T Finance Limited (formerly known as L&T Finance Holdings Limited) is one of the leading Non-Banking Financial Companies in India, with a strong presence across the lending business catering to the diverse financing needs of served and underserved customers.
                         L&T Finance offers Personal loans, Two-Wheeler loans, Home loans, Rural Business loans, Farm loans, and SME loans. At L&T Finance, we are committed to providing customised financial solutions that meet the unique needs of our customers. With a focus on innovation and customer-centricity, we aim to be a trusted partner in our customers' financial journey.
                     </p>
                 </div>
-                <img src={collab} className='h-[400px] max-sm:h-[300px]'/>
+                <img src={collab} className='h-[400px] mt-10 max-sm:h-[300px] md:mt-0 hidden md:block'/>
             </div>
             
         </div>
@@ -66,17 +69,17 @@ function ServiceContent() {
          flex justify-around text-center max-sm:flex-col max-sm:items-center space-x-2 max-sm:p-4 max-sm:space-y-4'>
             <div className=''>
                 <img src={personalLoan} className='h-[100px]' />
-                <p className='text-lg font-bold '>Personal Loan</p>
+                <p className='text-lg font-bold -ml-2'>Personal Loan</p>
                 {/* <p className='text-md font-normal'>Created to provide capital to small businesses. Evaluates business’ actual performance.</p> */}
             </div>
             <div>
                 <img src={business} className='h-[100px]'/>
-                <p className='text-lg font-bold'>Business Loan</p>
+                <p className='text-lg font-bold -ml-2'>Business Loan</p>
                 {/* <p className='text-md font-normal'>Created to provide capital to small businesses. Evaluates business’ actual performance.</p> */}
             </div>
             <div>
                 <img src={mortgageLoan} className='h-[100px]'/>
-                <p className='text-lg font-bold'>Mortgage Loan</p>
+                <p className='text-lg font-bold -ml-2'>Mortgage Loan</p>
                 {/* <p className='text-md font-normal'>Created to provide capital to small businesses. Evaluates business’ actual performance.</p> */}
             </div>
             <div>
@@ -87,14 +90,9 @@ function ServiceContent() {
         </div>
         {/* financial problems */}
         <div className='bg-white p-4 px-[30px] py-[50px] text-black flex justify-between max-sm:p-4 max-sm:flex-col'>
-            <div className=''>
             <h1 className='text-3xl font-semibold'>Ready to solve your financial problem?</h1>
-            <p className='text-lg font-normal'>It’s free—prequalify without affecting your credit. It’s free—prequalify 
-            without affecting your credit!</p>
-            </div>
-            <button className='ring-2 ring-[#11AAA2] rounded-[30px] bg-[#11AAA2] flex justify-center text-lg font-normal w-[20%] mt-[30px] max-sm:w-[50%] text-white'>
-                    Get Your Loan Now
-                    
+            <button className='ring-2 ring-[#11AAA2] rounded-[30px] bg-[#11AAA2] flex justify-center text-lg font-normal w-[20%] mt-[30px] md:mt-0 max-sm:w-[50%] text-white'>
+                    Get Your Loan Now          
             </button>
         </div>
         {/* footer */}
